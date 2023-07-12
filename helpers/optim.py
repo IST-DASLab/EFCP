3482c9b802cf97823392ac90d70e162499bf2213
Ionut-Vlad MODORANU <modoranu.ionut.vlad@hotmail.com>
2023-07-12 17:37:46 +0200

    add default wd 0 when group does not have weight_decay key

--- helpers/optim.py
+++ helpers/optim.py
@@ -82,7 +82,7 @@ def update_model(params, update, wd_type, alpha=None):
     count = 0
     for group in params:
         lr = group['lr']
-        wd = group['weight_decay']
+        wd = group.get('weight_decay', 0)
         # lr_wd = lr * wd
         for p in group['params']:
             u = update[count:(count + p.numel())].reshape(p.shape).to(p.device) # move the update from its custom GPU to the device of parameter
